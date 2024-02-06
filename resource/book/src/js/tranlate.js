8bd83c767b5625e2fa3b1c8a28c8ccca35b844f9
scg130 <scg130@163.com>
2024-02-06 10:12:56 +0800

    1

--- resource/book/src/js/tranlate.js
+++ resource/book/src/js/tranlate.js
@@ -5,12 +5,7 @@ script.src= 'https://res.zvo.cn/translate/inspector_v2.js';
 script.onload = script.onreadystatechange = function() {
 	translate.localLanguage = "en";
 	translate.selectLanguageTag.languages = 'zh-CN,zh-TW,en,ko';
-	translate.execute();
-	document.getElementById('translate').style.position = 'fixed';
-	document.getElementById('translate').style.color = 'red';
-	document.getElementById('translate').style.right = '1px';
-	document.getElementById('translate').style.top = '45px';
-	document.getElementById('translate').style.fontSize = '14px';
 	document.getElementById('translate').style.zIndex = '9999999999999';
+	translate.execute();
 }
 head.appendChild(script);
